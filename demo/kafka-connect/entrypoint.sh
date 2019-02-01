810e0816dd9ff51e6386d77ce24b6ac910486606
Clément Poulain <clement.poulain@hpe.com>
2019-02-01 17:57:07 +0100

    Rework entrypoint.sh to run configuration in background

--- demo/kafka-connect/entrypoint.sh
+++ demo/kafka-connect/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-/etc/confluent/docker/run &
-
-# wait for the REST API to be up
-until curl connect:8083/connectors; do
+wait_and_configure(){
+  # wait for the REST API to be up
+  until curl connect:8083/connectors; do
 	sleep 5
-done
+  done
 
-/etc/kafka-connect/configure-mqtt-fabric.sh
+  /etc/kafka-connect/configure-mqtt-fabric.sh
+}
+
+wait_and_configure &
+
+/etc/confluent/docker/run
