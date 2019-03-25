8bd9f65594a0487ddc6feff8b10b182b3e60de05
jhanson <jhanson@sgi.com>
2019-03-25 17:46:49 -0400

    Add source and sink connectors for registration

--- demo/kafka-connect/configure-mqtt-fabric.sh
+++ demo/kafka-connect/configure-mqtt-fabric.sh
@@ -5,6 +5,37 @@ echo "creating Kafka topic"
 kafka-topics --zookeeper zookeeper:2181 --topic fabric --partitions 3 --replication-factor 3 --create
 
 echo ""
+echo "removing MQTT Sink Connector"
+
+curl -X DELETE "connect:8083/connectors/mqtt-sink-reg-res"
+
+echo ""
+echo "creating MQTT Sink Connector"
+
+curl -X POST "connect:8083/connectors" \
+     -H "Content-Type: application/json" \
+     -d $'{
+  "name": "mqtt-sink-reg-res",
+  "config": {
+    "connector.class": "com.datamountaineer.streamreactor.connect.mqtt.sink.MqttSinkConnector",
+    "tasks.max": "1",
+    "topics": "registration-result",
+    "connect.mqtt.connection.clean": "true",
+    "connect.mqtt.connection.timeout": "1000",
+    "connect.mqtt.kcql": "INSERT INTO registration-result SELECT * FROM registration-result WITHCONVERTER=`com.datamountaineer.streamreactor.connect.converters.source.JsonSimpleConverter`",
+    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "key.converter.schemas.enable": "false",
+    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "value.converter.schemas.enable": "false",
+    "connect.mqtt.connection.keep.alive": "1000",
+    "connect.mqtt.client.id": "mqtt-connect-sink",
+    "connect.mqtt.converter.throw.on.error": "true",
+    "connect.mqtt.hosts": "tcp://mosquitto:1883",
+    "connect.mqtt.service.quality": "2"
+  }
+  }'
+
+echo ""
 echo "removing MQTT IBswitch and Registration Source Connector"
 
 curl -X DELETE "connect:8083/connectors/mqtt-source-ibswitch"
@@ -47,43 +78,13 @@ curl -X POST "connect:8083/connectors" \
     "tasks.max": "1",
     "connect.mqtt.connection.clean": "true",
     "connect.mqtt.connection.timeout": "1000",
-    "connect.mqtt.kcql": "INSERT INTO registration_request SELECT * FROM registration_request WITHCONVERTER=`com.datamountaineer.streamreactor.connect.converters.source.JsonSimpleConverter`",
+    "connect.mqtt.kcql": "INSERT INTO registration-request SELECT * FROM registration-request WITHCONVERTER=`com.datamountaineer.streamreactor.connect.converters.source.JsonSimpleConverter`",
     "key.converter": "org.apache.kafka.connect.json.JsonConverter",
     "key.converter.schemas.enable": "false",
     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
     "value.converter.schemas.enable": "false",
     "connect.mqtt.connection.keep.alive": "1000",
     "connect.mqtt.client.id": "mqtt-connect-02",
-    "connect.mqtt.converter.throw.on.error": "true",
-    "connect.mqtt.hosts": "tcp://mosquitto:1883",
-    "connect.mqtt.service.quality": "2"
-  }
-  }'
-
-echo ""
-echo "removing MQTT Sink Connector"
-
-curl -X DELETE "connect:8083/connectors/mqtt-sink"
-
-echo ""
-echo "creating MQTT Sink Connector"
-
-curl -X POST "connect:8083/connectors" \
-     -H "Content-Type: application/json" \
-     -d $'{
-  "name": "mqtt-sink",
-  "config": {
-    "connector.class": "com.datamountaineer.streamreactor.connect.mqtt.sink.MqttSinkConnector",
-    "tasks.max": "1",
-    "connect.mqtt.connection.clean": "true",
-    "connect.mqtt.connection.timeout": "1000",
-    "connect.mqtt.kcql": "INSERT INTO registration_result SELECT * FROM registration_result WITHCONVERTER=`com.datamountaineer.streamreactor.connect.converters.source.JsonSimpleConverter`",
-    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
-    "key.converter.schemas.enable": "false",
-    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    "value.converter.schemas.enable": "false",
-    "connect.mqtt.connection.keep.alive": "1000",
-    "connect.mqtt.client.id": "mqtt-connect-sink",
     "connect.mqtt.converter.throw.on.error": "true",
     "connect.mqtt.hosts": "tcp://mosquitto:1883",
     "connect.mqtt.service.quality": "2"
