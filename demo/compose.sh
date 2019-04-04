95291942d65bc8b1b69ea79cee1d6d31817ec726
seb <sebastien.cabaniols@hpe.com>
2019-04-04 09:02:21 +0200

    Revert "Improve proxy detection using wget instead of ping"
    This reverts commit 9eb2d8ce6bef0442151689f1a1ca766c0b91a0d8.

--- demo/compose.sh
+++ demo/compose.sh
@@ -6,14 +6,10 @@ files="-f kafka-compose.yml -f sim-druid-grafana-compose.yml -f connect-compose.
 
 if [ "$action" == "up" ]; then
 	echo "Checking whether we are on the HPE LAN and needing a proxy..."
-	if [ ! -x /usr/bin/wget ]; then 
-		echo "Unable to find wget in your env, intsall it to have automatic HPE proxy detection"
-	else
-		wget -q --dns-timeout=2 autocache.hpecorp.net -O /dev/null
-		if [ $? -eq 0 ]; then
-			files="${files} -f docker-proxy.yml"
-			echo "HPE proxies set up"
-		fi
+	ping -c 1 autocache.hpecorp.net 2>&1 >/dev/null
+	if [ $? -eq 0 ]; then
+		files="${files} -f docker-proxy.yml"
+		echo "HPE proxies set up"
 	fi
 
 	docker-compose ${files} up --build --remove-orphans -d
