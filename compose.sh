17bcba2b0a856604e638ae52b90828002247df4d
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2019-04-05 16:06:26 +0200

    remove demo folder and align all components in root folder

--- compose.sh
+++ compose.sh
@@ -2,7 +2,8 @@
 
 action=$1
 
-files="-f kafka-compose.yml -f sim-druid-grafana-compose.yml -f connect-compose.yml -f framework-redis-compose.yml"
+export COMPOSE_PROJECT_NAME=demo
+export COMPOSE_FILE=kafka-compose.yml:sim-druid-grafana-compose.yml:connect-compose.yml:framework-redis-compose.yml
 
 if [ "$action" == "up" ]; then
 	echo "Checking whether we are on the HPE LAN and needing a proxy..."
@@ -11,16 +12,16 @@ if [ "$action" == "up" ]; then
 	else
 		wget -q --dns-timeout=2 autocache.hpecorp.net -O /dev/null
 		if [ $? -eq 0 ]; then
-			files="${files} -f docker-proxy.yml"
+			export COMPOSE_FILE=${COMPOSE_FILE}:docker-proxy.yml
 			echo "HPE proxies set up"
 		fi
 	fi
 
-	docker-compose ${files} up --build --remove-orphans -d
+	docker-compose up --build --remove-orphans -d
 elif [ "$action" == "down" ]; then
-    docker-compose ${files} down --remove-orphans
+	docker-compose down --remove-orphans
 else
-	 echo "Unknown action"
-	 exit 1
+	echo "Unknown action"
+	exit 1
 fi
 
