98809765a1759a804a28f76a52017296ae5922a3
jeff.hanson <jeff.hanson@hpe.com>
2019-07-19 11:04:30 -0400

    Fix broker-1 to broker-2 and broker-3 on the last two lines

--- test-tools/check-pipeline.sh
+++ test-tools/check-pipeline.sh
@@ -46,8 +46,8 @@ run_me druid_perfquery  'curl -s -X 'POST' -H 'Content-Type:application/json' -d
 run_me druid_collectd   'curl -s -X 'POST' -H 'Content-Type:application/json' -d @/tmp/druid_collectd.json   http://druid:8082/druid/v2?pretty | grep timestamp' || exit 1
 run_me influxdb       timeout 10 'curl -G "http://influxdb:8086/query?pretty=true" --data-urlencode "db=datapipes" --data-urlencode "q=show diagnostics"' || exit 1
 run_me ssl-broker-1   kafkacat -L -X ssl.ca.location=/run/secrets/bd-ca-1.crt -X ssl.certificate.location=/run/secrets/client.certificate.pem -X ssl.key.location=/run/secrets/client.key -X ssl.key.password=bluedragon -X security.protocol=ssl -b broker-1:19092 || exit 1
-run_me ssl-broker-2   kafkacat -L -X ssl.ca.location=/run/secrets/bd-ca-1.crt -X ssl.certificate.location=/run/secrets/client.certificate.pem -X ssl.key.location=/run/secrets/client.key -X ssl.key.password=bluedragon -X security.protocol=ssl -b broker-1:19093 || exit 1
-run_me ssl-broker-3   kafkacat -L -X ssl.ca.location=/run/secrets/bd-ca-1.crt -X ssl.certificate.location=/run/secrets/client.certificate.pem -X ssl.key.location=/run/secrets/client.key -X ssl.key.password=bluedragon -X security.protocol=ssl -b broker-1:19094 || exit 1
+run_me ssl-broker-2   kafkacat -L -X ssl.ca.location=/run/secrets/bd-ca-1.crt -X ssl.certificate.location=/run/secrets/client.certificate.pem -X ssl.key.location=/run/secrets/client.key -X ssl.key.password=bluedragon -X security.protocol=ssl -b broker-2:19093 || exit 1
+run_me ssl-broker-3   kafkacat -L -X ssl.ca.location=/run/secrets/bd-ca-1.crt -X ssl.certificate.location=/run/secrets/client.certificate.pem -X ssl.key.location=/run/secrets/client.key -X ssl.key.password=bluedragon -X security.protocol=ssl -b broker-3:19094 || exit 1
 
 echo ""
 echo "all tests succeeded..."
