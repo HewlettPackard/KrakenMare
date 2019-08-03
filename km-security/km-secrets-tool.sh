d6884b928133c038ababa7fc2e16290fc69e3c4b
jeff.hanson <jeff.hanson@hpe.com>
2019-08-03 09:22:42 -0400

    Add client-sasl_ssl.conf to containers as a secret

--- km-security/km-secrets-tool.sh
+++ km-security/km-secrets-tool.sh
@@ -128,7 +128,7 @@ function push_to_swarm ()
 }
 
 #list of secrets
-file_list="km-ca-1.key km-ca-1.crt km-ca-1.srl broker_jaas.conf zookeeper_jaas.conf"
+file_list="km-ca-1.key km-ca-1.crt km-ca-1.srl broker_jaas.conf zookeeper_jaas.conf client-sasl_ssl.conf"
 for i in broker-1 broker-2 broker-3 schemaregistry connect client
 do
     file_list="$file_list ${i}_sslkey_creds"
