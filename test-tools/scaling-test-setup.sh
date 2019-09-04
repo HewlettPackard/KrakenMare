d12b0647913388b4670554210ef7f7f4f56c8b91
jeff.hanson <jeff.hanson@hpe.com>
2019-09-04 13:20:12 -0400

    Add script to configure cluster for test

---
+++ test-tools/scaling-test-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Setup for scaling test. Run on ONE instance only and only once.
+#
+
+export  KAFKA_OPTS="-Djava.security.auth.login.config=/run/secrets/broker_jaas.conf"
+# In the test below we are sending what Kafka by default considers extra large records (the defaults are roughly 1M)
+# To do this we adjust topic to have a size of 9 million, the brokers to support 9 million and producer to support 9 million
+# producer configuration is in /tmp/client-sasl_ssl.conf
+kafka-topics --bootstrap-server broker-1:29092 --delete --topic sasl_ssl  --command-config /tmp/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --create --topic sasl_ssl --partitions 5 --replication-factor 2 --config max.message.bytes=9000000 --command-config /tmp/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --topic sasl_ssl --describe --command-config /tmp/client-sasl_ssl.conf || exit 1
+
+kafka-configs --bootstrap-server broker-1:29092 --entity-default  --entity-type brokers --alter --add-config message.max.bytes=9000000  --command-config /tmp/client-sasl_ssl.conf || exit 1
+kafka-configs --bootstrap-server broker-1:29092 --entity-type brokers --describe --entity-default --command-config /tmp/client-sasl_ssl.conf || exit 1
