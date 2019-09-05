36947299f9e7f91d304332d3b797f0d0a69e9a23
jeff.hanson <jeff.hanson@hpe.com>
2019-09-04 22:59:34 -0400

    Keep going if topic doesn't exist before deleting

--- test-tools/scaling-test-setup.sh
+++ test-tools/scaling-test-setup.sh
@@ -7,7 +7,7 @@ export  KAFKA_OPTS="-Djava.security.auth.login.config=/run/secrets/broker_jaas.c
 # In the test below we are sending what Kafka by default considers extra large records (the defaults are roughly 1M)
 # To do this we adjust topic to have a size of 9 million, the brokers to support 9 million and producer to support 9 million
 # producer configuration is in /tmp/client-sasl_ssl.conf
-kafka-topics --bootstrap-server broker-1:29092 --delete --topic sasl_ssl  --command-config /tmp/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --delete --topic sasl_ssl  --command-config /tmp/client-sasl_ssl.conf
 kafka-topics --bootstrap-server broker-1:29092 --create --topic sasl_ssl --partitions 5 --replication-factor 2 --config max.message.bytes=9000000 --command-config /tmp/client-sasl_ssl.conf || exit 1
 kafka-topics --bootstrap-server broker-1:29092 --topic sasl_ssl --describe --command-config /tmp/client-sasl_ssl.conf || exit 1
 
