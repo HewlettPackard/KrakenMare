071383ce403ae5ed130d18e45943dbd7c3043557
jeff.hanson <jeff.hanson@hpe.com>
2019-10-02 12:23:36 -0400

    Add wait-for brokers before starting

--- redfish/redfish-sensors.sh
+++ redfish/redfish-sensors.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Wait for all three brokers to be up
+/tmp/wait-for --timeout=240 broker-1:9092 || exit 1
+/tmp/wait-for --timeout=240 broker-2:9093 || exit 1
+/tmp/wait-for --timeout=240 broker-3:9094 || exit 1
+
 env
 COMMAND="/bin/true"
 if [ "x$REDFISHIP" = "x" ]; then 
@@ -15,7 +20,9 @@ if [ "x$REDFISHPWD" = "x" ]; then
 	COMMAND="/bin/bash"
 fi
 if [ "$COMMAND" != "/bin/bash" ]; then 
+	echo "Run redfish-client"
 	redfish-client config add dl560g10 https://$REDFISHIP/redfish/v1 "$REDFISHACC" "$REDFISHPWD"
+	echo "Run redfish-sensors.py"
 	python2 /redfish-sensors.py
 else
 	$COMMAND
