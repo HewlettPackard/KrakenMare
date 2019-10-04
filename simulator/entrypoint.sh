04684ad20d99dbb474238a2eb9b5035eb854c11b
seb <sebastien.cabaniols@hpe.com>
2019-10-04 17:27:02 -0400

    make simulator wait on mqtt+framework+schemaregistry

--- simulator/entrypoint.sh
+++ simulator/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Wait for all three brokers to be up
-/tmp/wait-for --timeout=240 broker-1:9092 || exit 1
-/tmp/wait-for --timeout=240 broker-2:9093 || exit 1
-/tmp/wait-for --timeout=240 broker-3:9094 || exit 1
+/tmp/wait-for --timeout=240 mosquitto:1883 || exit 1
+/tmp/wait-for --timeout=240 framework:8080 || exit 1
+/tmp/wait-for --timeout=240 schemaregistry:8081 || exit 1
 
 cd /simulator
 python3 IBswitchSimulator.py --mode=mqtt
+sleep 1000000000
