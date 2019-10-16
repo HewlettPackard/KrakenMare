5ddeb46a4a3ab4448a5fabb256f5eadf1d61a8bb
Bruno Cornec <bruno.cornec@hpe.com>
2019-10-16 19:24:18 +0200

    Move to python3 for the redfish setup - Review python modules, secrets, networks needed - Simplify launch shell script and setup defaults - Fix serialization, uuid and avro process by casting to   correct value types (float for sensor values, int for timestamp) - Fix mqtt topic to make it simpler - Avoid exiting on error, loop instead

--- redfish/redfish-sensors.py
+++ redfish/redfish-sensors.py
@@ -1,39 +1,124 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Licensed under the APache v2 license
+# Copyright Hewlett-Packard Enterprise, 2019
+
 import os
+import sys
+import io
 import time
 import json
-from kafka import KafkaProducer
-from kafka.errors import KafkaUnavailableError
+from datetime import datetime
+#from kafka import KafkaProducer
+#from kafka.errors import KafkaUnavailableError
 import subprocess
+import paho.mqtt.client as mqtt
+from fastavro import schemaless_writer, schemaless_reader
+from schema_registry.client import SchemaRegistryClient
+from schema_registry.serializers import MessageSerializer
+import redfish
+import uuid
+import hashlib
 
 # Time to wait between requests
-SLEEP = 60
-kafka_broker = "broker-1:9092"
+SLEEP = 120
 
-# wait for Kafka broker to answer
-producer = None
+conf = {
+   "url": "https://schemaregistry:8081",
+   "ssl.ca.location": "/run/secrets/km-ca-1.crt",
+   "ssl.certificate.location":
+   "/run/secrets/schemaregistry.certificate.pem",
+   "ssl.key.location": "/run/secrets/schemaregistry.key",
+}
 
-while not producer:
-    time.sleep(5)
-    try:
-        producer = KafkaProducer(bootstrap_servers=[kafka_broker],value_serializer=lambda m: json.dumps(m).encode('utf8'))
-    except KafkaUnavailableError:
-        print "waiting for Kafka broker..."
+client = SchemaRegistryClient(conf)
+subject = "com-hpe-krakenmare-message-agent-send-time-series"
+cg = None
+
+while cg is None:
+    cg = client.get_schema(subject)
+    print("getting schema %s from schemaregistry" % subject)
+    time.sleep(1)
+
+# Hardcoded for now
+mqtt_broker = "mosquitto"
+mqtt_port = 1883
+# Faked UUID
+mqttuuid = "a8098c1a-f86e-11da-bd1a-00112444be1e"
+machine = str(os.environ['REDFISHIP'])
+topic = "redfish/" + machine.split("://")[1].split("/")[0] + '/'
+mqttclient = mqtt.Client(mqttuuid)
+print("connecting to mqtt broker")
+mqttclient.connect(mqtt_broker, mqtt_port)
 
 # Infinite loop
-rfdict = {}
+i = 0
 while True:
-    # Read formatted data from the server's chassis
-    for line in subprocess.check_output(['redfish-client', '--insecure', 'chassis', 'getinfo', 'dl560g10']).splitlines():
-        print('line: %s'%line)
-        if ':' in line:
-            key = line.split(":")[0]
-            value = line.split(":")[1]
-            print('key: %s value: %s'%(key.strip(),value.strip()))
-            rfdict[key.strip()] = value.strip()
+    i = i + 1
+    if os.environ['REDFISHACC'] == "":
+        simulator = True
+        enforceSSL = False
+    else:
+        simulator = False
+        enforceSSL = True
+    try:
+        redfish_data = redfish.connect(os.environ['REDFISHIP'],
+                                    os.environ['REDFISHACC'],
+                                    os.environ['REDFISHPWD'],
+                                    verify_cert=False,
+                                    simulator=simulator,
+                                    enforceSSL=enforceSSL)
+    except redfish.exception.RedfishException as e:
+            sys.stderr.write(str(e.message))
+            sys.stderr.write(str(e.advices))
+            print("Sleeping " + str(SLEEP) + " seconds")
+            time.sleep(SLEEP)
+            continue
 
     # Write the json data to mqtt broker
-    print('Publishing via Kafka')
-    producer.send('ilo', rfdict)
+    measurementList = []
+    for chassis in redfish_data.Chassis.chassis_dict.values():
+        name = chassis.get_name()
+        for sensor, temp in chassis.thermal.get_temperatures().items():
+            # Using https://docs.python.org/3/library/uuid.html
+            s = name + '-' + sensor
+            uu = uuid.UUID(hashlib.md5(s.encode()).hexdigest())
+            measurementList.append(
+                    { "sensorUUID": uu,
+                        "sensorValue": float(temp) })
+        for fan, rpm in chassis.thermal.get_fans().items():
+            rpml = rpm.split(None, 1)
+            rpm = rpml[0]
+            s = name + '-' + fan
+            uu = uuid.UUID(hashlib.md5(s.encode()).hexdigest())
+            measurementList.append(
+                    { "sensorUUID": uu,
+                        "sensorValue": float(rpm) })
+        #for ps, volt in chassis.power.get_power().items():
+            #measurementList.append(
+                    #{ "sensorUUID": name + '-' + ps,
+                        #"sensorValue": volt })
 
-    # Infinite loop - Redfish is slow so wait 1 min'
+    record = {
+        "uuid": str(mqttuuid),
+        "timestamp": int(round(datetime.timestamp(datetime.now())*1000)),
+        "measurementList": measurementList
+        }
+
+    for t in measurementList:
+        for k,v in t.items():
+            # remove that comment to check the sensors
+            # print("key: " + str(k) + " value: " + str(v))
+            pass
+
+    sts = MessageSerializer(client)
+    raw_bytes = sts.encode_record_with_schema_id(
+                        cg.schema_id, record
+                    )
+    print(str(i) + ":Publishing via mqtt (topic:" + str(topic) + ") uuid: " + str(mqttuuid))
+    mqttclient.publish(topic, raw_bytes)
+
+    # Infinite loop - Redfish is slow so wait
+    print("Sleeping " + str(SLEEP) + " seconds")
     time.sleep(SLEEP)
--- redfish/redfish-sensors.sh
+++ redfish/redfish-sensors.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 # Wait for all three brokers to be up
-/tmp/wait-for --timeout=240 broker-1:9092 || exit 1
-/tmp/wait-for --timeout=240 broker-2:9093 || exit 1
-/tmp/wait-for --timeout=240 broker-3:9094 || exit 1
+/tmp/wait-for --timeout=240 mosquitto:1883 || exit 1
+/tmp/wait-for --timeout=240 framework:8080 || exit 1
+/tmp/wait-for --timeout=240 schemaregistry:8081 || exit 1
 
-env
+# Hardcoded for now if not defined
+export REDFISHIP=${REDFISHIP:="https://16.19.180.65/redfish/v1"}
+export REDFISHACC=${REDFISHACC:="cmu"}
+export REDFISHPWD=${REDFISHPWD:="HPinvent"}
+
+# Reset values for HPE simulator
+if [ "$REDFISHIP" = "https://ilorestfulapiexplorer.ext.hpe.com/redfish/v1" ]; then
+	export REDFISHACC=""
+	export REDFISHPWD=""
+fi
+
+# Useless when harcoding values but keeping for now
 COMMAND="/bin/true"
 if [ "x$REDFISHIP" = "x" ]; then 
 	echo "REDFISHIP not provided - will run bash"
 	COMMAND="/bin/bash"
 fi
-if [ "x$REDFISHACC" = "x" ]; then 
-	echo "REDFISHACC not provided - will run bash"
-	COMMAND="/bin/bash"
-fi
-if [ "x$REDFISHPWD" = "x" ]; then 
-	echo "REDFISHPWD not provided - will run bash"
-	COMMAND="/bin/bash"
-fi
 if [ "$COMMAND" != "/bin/bash" ]; then 
-	echo "Run redfish-client"
-	redfish-client config add dl560g10 https://$REDFISHIP/redfish/v1 "$REDFISHACC" "$REDFISHPWD"
 	echo "Run redfish-sensors.py"
-	python2 /redfish-sensors.py
+	/redfish-sensors.py
+	sleep infinity
 else
 	$COMMAND
 fi
