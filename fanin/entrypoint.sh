d274c9c5ac7aeff036fc27049ff8028f2b81af8a
seb <sebastien.cabaniols@hpe.com>
2019-10-22 09:36:34 -0400

    added sleep infinity + wait-for entrypoint for fanin

---
+++ fanin/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Wait for all three brokers to be up
+/tmp/wait-for --timeout=240 mosquitto:1883 || exit 1
+/tmp/wait-for --timeout=240 framework:8080 || exit 1
+/tmp/wait-for --timeout=240 broker-1:9092 || exit 1
+/tmp/wait-for --timeout=240 broker-2:9093 || exit 1
+/tmp/wait-for --timeout=240 broker-3:9094 || exit 1
+
+cd /fanin
+python3 FanIn.py
+sleep infinity
