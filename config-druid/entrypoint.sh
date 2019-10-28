5928ed1f98cef029a02fddbb599d41df23956276
jeff.hanson <jeff.hanson@hpe.com>
2019-10-28 15:28:48 -0400

    Clean up the printing to logs

--- config-druid/entrypoint.sh
+++ config-druid/entrypoint.sh
@@ -7,9 +7,9 @@ until curl druid:8090/druid/indexer/v1/supervisor; do
   sleep 5
 done
 
-# Load the indexer for simulator data
-curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/perfquery-index.json http://druid:8090/druid/indexer/v1/supervisor
+echo "Load the indexer for simulator data"
+curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/perfquery-index.json http://druid:8090/druid/indexer/v1/supervisor || exit 1
 curl -s druid:8090/druid/indexer/v1/supervisor | jq .
-# Load the indexer for collectd data
+echo " Load the indexer for collectd data"
 curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/collectd-index.json http://druid:8090/druid/indexer/v1/supervisor || exit 1
 curl -s druid:8090/druid/indexer/v1/supervisor | jq .
