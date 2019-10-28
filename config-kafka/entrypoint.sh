750e7a9f73d008ef71cefee78724249f65dd9fa2
jeff.hanson <jeff.hanson@hpe.com>
2019-10-28 15:29:26 -0400

    Wait for the right named brokers

--- config-kafka/entrypoint.sh
+++ config-kafka/entrypoint.sh
@@ -3,7 +3,7 @@
 /tmp/wait-for --timeout=240 zookeeper:2181 || exit 1
 /tmp/wait-for --timeout=240 broker-1:9092 || exit 1
 /tmp/wait-for --timeout=240 broker-2:9093 || exit 1
-/tmp/wait-for --timeout=240 broker-4:9094 || exit 1
+/tmp/wait-for --timeout=240 broker-3:9094 || exit 1
 
 kafka-topics --zookeeper zookeeper:2181 --topic fabric --create --partitions 10 --replication-factor 2
 kafka-topics --zookeeper zookeeper:2181 --topic fabric --describe
