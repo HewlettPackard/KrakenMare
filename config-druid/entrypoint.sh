af4fc4f0da860cdaf29c8aa4e902528def4cb0fb
jeff.hanson <jeff.hanson@hpe.com>
2019-10-28 15:11:31 -0400

    Fix to have correct packages and copy entrypoint in Dockerfile.  Redo logic of entrypoint to be simpler

--- config-druid/entrypoint.sh
+++ config-druid/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+set -x
 
-wait_and_configure_druid_supervisor(){
-  # wait for supervisor rest API to be up
-  until curl druid:8090/druid/indexer/v1/supervisor; do
-     sleep 5
-  done
+/tmp/wait-for --timeout=240 druid:8090 || exit 1
 
-  # Load the indexer for simulator data
-  curl -XPOST -H'Content-Type: application/json' -d @/tmp/perfquery-index.json http://druid:8090/druid/indexer/v1/supervisor
-  # Load the indexer for collectd data
-  curl -XPOST -H'Content-Type: application/json' -d @/tmp/collectd-index.json http://druid:8090/druid/indexer/v1/supervisor
+# wait for supervisor rest API to be up
+until curl druid:8090/druid/indexer/v1/supervisor; do
+  sleep 5
+done
 
-}
-
-wait_and_configure_druid_supervisor &
+# Load the indexer for simulator data
+curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/perfquery-index.json http://druid:8090/druid/indexer/v1/supervisor
+curl -s druid:8090/druid/indexer/v1/supervisor | jq .
+# Load the indexer for collectd data
+curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/collectd-index.json http://druid:8090/druid/indexer/v1/supervisor || exit 1
+curl -s druid:8090/druid/indexer/v1/supervisor | jq .
