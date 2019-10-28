ae74fdcb6d9ca9f3485c531f9b10a6450745b152
jeff.hanson <jeff.hanson@hpe.com>
2019-10-28 15:34:10 -0400

    More cleanup of printing

--- config-druid/entrypoint.sh
+++ config-druid/entrypoint.sh
@@ -3,7 +3,7 @@
 /tmp/wait-for --timeout=240 druid:8090 || exit 1
 
 # wait for supervisor rest API to be up
-until curl druid:8090/druid/indexer/v1/supervisor; do
+until curl -s druid:8090/druid/indexer/v1/supervisor; do
   sleep 5
 done
 
