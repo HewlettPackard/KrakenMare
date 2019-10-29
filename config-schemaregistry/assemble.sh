e45e11b8ba666f02d4084cce8b99a9bbaacbae76
jeff.hanson <jeff.hanson@hpe.com>
2019-10-29 09:02:26 -0400

    Make uuids for sensors into optional.

--- config-schemaregistry/assemble.sh
+++ config-schemaregistry/assemble.sh
@@ -3,10 +3,13 @@
 cp framework/com.hpe.krakenmare.core-sensor.avsc com.hpe.krakenmare.core-agent.avsc
 cat framework/com.hpe.krakenmare.core-device.avsc >> com.hpe.krakenmare.core-agent.avsc
 cat framework/com.hpe.krakenmare.core-agent.avsc >> com.hpe.krakenmare.core-agent.avsc
+sed -i s'/"type": {"type": "string", "logicalType":"uuid"}/"type": ["null", {"type": "string", "logicalType":"uuid"}]/' com.hpe.krakenmare.core-agent.avsc
 
 cp framework/com.hpe.krakenmare.core-sensor.avsc com.hpe.krakenmare.core-device.avsc
 cat framework/com.hpe.krakenmare.core-device.avsc >> com.hpe.krakenmare.core-device.avsc
+sed -i s'/"type": {"type": "string", "logicalType":"uuid"}/"type": ["null", {"type": "string", "logicalType":"uuid"}]/' com.hpe.krakenmare.core-device.avsc
 
 cp framework/com.hpe.krakenmare.core-sensor.avsc com.hpe.krakenmare.message.agent-device.list.avsc
 cat framework/com.hpe.krakenmare.core-device.avsc >> com.hpe.krakenmare.message.agent-device.list.avsc
 cat framework/com.hpe.krakenmare.message.agent-device.list.avsc >> com.hpe.krakenmare.message.agent-device.list.avsc
+sed -i s'/"type": {"type": "string", "logicalType":"uuid"}/"type": ["null", {"type": "string", "logicalType":"uuid"}]/' com.hpe.krakenmare.message.agent-device.list.avsc
