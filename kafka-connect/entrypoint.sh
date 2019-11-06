93f7da74ed513be7024784cb7f1e78fc05225459
jeff.hanson <jeff.hanson@hpe.com>
2019-11-06 14:26:02 -0500

    Remove extra features from kafka-connect

--- kafka-connect/entrypoint.sh
+++ kafka-connect/entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-wait_for_connect_REST_API(){
-  # wait for connect REST API to be up
-  until curl connect:8083/connectors; do
-    sleep 5
-  done
-
-}
-
 /tmp/wait-for --timeout=240 broker-1:9092 || exit 1
 /tmp/wait-for --timeout=240 broker-2:9093 || exit 1
 /tmp/wait-for --timeout=240 broker-3:9094 || exit 1
-
-wait_for_connect_REST_API &
 
 /etc/confluent/docker/run
