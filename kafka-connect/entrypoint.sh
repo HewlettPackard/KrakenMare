70c2a173ad9de522035dda0bc6d653569caa5687
jeff.hanson <jeff.hanson@hpe.com>
2019-11-07 07:43:17 -0500

    Remove unused kafka-connect

--- kafka-connect/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/tmp/wait-for --timeout=240 broker-1:9092 || exit 1
-/tmp/wait-for --timeout=240 broker-2:9093 || exit 1
-/tmp/wait-for --timeout=240 broker-3:9094 || exit 1
-
-/etc/confluent/docker/run
