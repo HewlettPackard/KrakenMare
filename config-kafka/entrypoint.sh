5e3241ec66087d82beb8d8e391101344dcb4440a
Jeff Hanson <jeff.hanson@hpe.com>
2019-12-06 14:27:39 +0100

    Add agent-registration and sensor-registration topics

--- config-kafka/entrypoint.sh
+++ config-kafka/entrypoint.sh
@@ -10,3 +10,11 @@ export  KAFKA_OPTS="-Djava.security.auth.login.config=/run/secrets/broker_jaas.c
 kafka-topics --bootstrap-server broker-1:29092 --delete --topic fabric  --command-config /run/secrets/client-sasl_ssl.conf
 kafka-topics --bootstrap-server broker-1:29092 --create --topic fabric --partitions 10 --replication-factor 2 --command-config /run/secrets/client-sasl_ssl.conf || exit 1
 kafka-topics --bootstrap-server broker-1:29092 --topic fabric --describe --command-config /run/secrets/client-sasl_ssl.conf || exit 1
+
+# Create registration topics
+kafka-topics --bootstrap-server broker-1:29092 --delete --topic agent-registration  --command-config /run/secrets/client-sasl_ssl.conf
+kafka-topics --bootstrap-server broker-1:29092 --create --topic agent-registration --config retention.ms=-1 --partitions 1 --replication-factor 3 --command-config /run/secrets/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --topic agent-registration --describe --command-config /run/secrets/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --delete --topic sensor-registration  --command-config /run/secrets/client-sasl_ssl.conf
+kafka-topics --bootstrap-server broker-1:29092 --create --topic sensor-registration --config retention.ms=-1  --partitions 1 --replication-factor 3 --command-config /run/secrets/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --topic sensor-registration --describe --command-config /run/secrets/client-sasl_ssl.conf || exit 1
