61f8ddd303993c1e998d29df7b486473d91a1290
jeff.hanson <jeff.hanson@hpe.com>
2019-12-06 10:03:46 -0500

    Add mqtt-benchmark to mqtt-bench.sh

--- test-tools/mqtt-bench.sh
+++ test-tools/mqtt-bench.sh
@@ -27,3 +27,6 @@ set -o nounset                              # Treat unset variables as an error
 /tmp/mqtt-bench -action=pub -broker="ssl://mosquittosecu:8883" -tls="client:/run/secrets/km-ca-1.crt,/run/secrets/mosquittosecu.certificate.pem,/run/secrets/mosquittosecu.key"
 /tmp/mqtt-bench -action=pub -broker="ssl://mosquittosecu:8883" -tls="client:/run/secrets/km-ca-1.crt,/run/secrets/mosquittosecu.certificate.pem,/run/secrets/mosquittosecu.key"  -clients=1 -qos=0 -count=100000 -size=131072
 
+# Method to use mqtt-benchmark
+mqtt-benchmark --broker=mqtt://mosquitto:1883 --clients 1 --count 5000 --size 131072
+mqtt-benchmark --broker=mqtts://mosquittosecu:8883 --ca /run/secrets/km-ca-1.crt --cert /run/secrets/mosquittosecu.certificate.pem --key /run/secrets/mosquittosecu.key   --clients 1 --count 5000 --size 131072
