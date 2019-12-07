c482010af0fdb247f086b9a4ebe45097d5d96527
Bruno Cornec <Bruno.Cornec@hpe.com>
2019-12-06 20:46:43 -0500

    Hello World approach
    - Create a KrakenMareTriplet python module and Class
    - Use it in the hello-world example
    - Needs to deal with time out on physical HW

---
+++ redfish/hello-world.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Wait for all three brokers to be up
+/tmp/wait-for --timeout=240 mosquitto:1883 || exit 1
+/tmp/wait-for --timeout=240 framework:8080 || exit 1
+/tmp/wait-for --timeout=240 schemaregistry:8081 || exit 1
+
+# Hardcoded for now if not defined
+export REDFISHIP=${REDFISHIP:="https://16.19.180.65/redfish/v1"}
+export REDFISHACC=${REDFISHACC:="cmu"}
+export REDFISHPWD=${REDFISHPWD:="HPinvent"}
+
+# Reset values for HPE simulator
+if [ "$REDFISHIP" = "https://ilorestfulapiexplorer.ext.hpe.com/redfish/v1" ]; then
+	export REDFISHACC=""
+	export REDFISHPWD=""
+fi
+
+# Useless when harcoding values but keeping for now
+COMMAND="/bin/true"
+if [ "x$REDFISHIP" = "x" ]; then 
+	echo "REDFISHIP not provided - will run bash"
+	COMMAND="/bin/bash"
+fi
+if [ "$COMMAND" != "/bin/bash" ]; then 
+	echo "Run hello-world.py"
+	/hello-world.py
+	#sleep infinity
+else
+	$COMMAND
+fi
