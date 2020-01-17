d29540d4749c21909f6ab2b70d7818de87a4f01a
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2020-01-17 14:49:43 +0100

    Apply suggestion to redfish/entrypoint.sh

--- redfish/entrypoint.sh
+++ redfish/entrypoint.sh
@@ -2,5 +2,5 @@
 
 #container is hot and ready, just start start.sh to do actual work.
 
-echo use: "cd /redfish && start.sh" to actually start the container payload.
+echo use: "/redfish/start.sh" to actually start the container payload.
 sleep infinity
