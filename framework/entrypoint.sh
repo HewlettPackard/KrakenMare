de09a358f7da71844b29e0ea34e2af01c5c8eaa3
Clément Poulain <clement.poulain@hpe.com>
2020-01-22 16:03:45 +0100

    Framework manager need to wait for schema registry

--- framework/entrypoint.sh
+++ framework/entrypoint.sh
@@ -4,6 +4,7 @@
     && /tmp/wait-for --timeout=240 broker-1:9092 \
     && /tmp/wait-for --timeout=240 broker-2:9093 \
     && /tmp/wait-for --timeout=240 broker-3:9094 \
-    && /tmp/wait-for --timeout=240 mosquitto:8883 -- java -cp target/framework-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.hpe.krakenmare.Main 
+    && /tmp/wait-for --timeout=240 mosquitto:8883 \
+    && /tmp/wait-for --timeout=240 schemaregistry:8081 -- java -cp target/framework-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.hpe.krakenmare.Main 
 
 
