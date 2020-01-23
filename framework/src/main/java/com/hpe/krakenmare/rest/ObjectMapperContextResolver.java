429cdf3cb9647c57a89fd437ec33e571896edb6d
Clément Poulain <clement.poulain@hpe.com>
2020-01-23 16:23:41 +0100

    Update JSON serialization config so we don't rely on POJO annotation

--- framework/src/main/java/com/hpe/krakenmare/rest/ObjectMapperContextResolver.java
+++ framework/src/main/java/com/hpe/krakenmare/rest/ObjectMapperContextResolver.java
@@ -1,5 +1,7 @@
 package com.hpe.krakenmare.rest;
 
+import java.io.IOException;
+
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
@@ -8,16 +10,39 @@ import org.apache.avro.util.Utf8;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Provider
 public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
 
 	// mixin to define Utf8(String string) as a JSON creator
+	@JsonSerialize(using = Utf8Serializer.class)
 	private static class Utf8Mixin {
 		@JsonCreator
 		public Utf8Mixin(String string) {
+		}
+	}
+
+	private static class Utf8Serializer extends JsonSerializer<Utf8> {
+		@Override
+		public void serialize(Utf8 value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+			provider.defaultSerializeValue(value.toString(), jgen);
+		}
+
+		@Override
+		public boolean isEmpty(SerializerProvider provider, Utf8 value) {
+			return value == null;
+		}
+
+		@Override
+		public Class<Utf8> handledType() {
+			return Utf8.class;
 		}
 	}
 
@@ -25,6 +50,7 @@ public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper
 	static {
 		MAPPER.configure(SerializationFeature.INDENT_OUTPUT, true);
 		MAPPER.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+		MAPPER.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
 		MAPPER.addMixIn(Utf8.class, Utf8Mixin.class);
 	}
 
