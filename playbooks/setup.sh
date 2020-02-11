e41d801bf3741f8a02d1550cf77f9b27b6d80208
Clément Poulain <clement.poulain@hpe.com>
2020-02-11 14:35:24 +0100

    Create directories before mounting them

--- playbooks/setup.sh
+++ playbooks/setup.sh
@@ -173,7 +173,8 @@ if [  "$ansible" == "1"  ]; then
 
          docker ps | grep docker-registry_registry- | awk '{ print $1}' | xargs docker stop
 
-         docker run --rm --volume /tmp/:/tmp/ --volume $KM_HOME:/playbooks/ --volume $KM_HOME/$DEFAULT_INVENTORY_FILE:/etc/ansible/hosts --volume $KM_HOME/../docker-registry:/docker-registry/  --network=host ansible ansible-playbook /playbooks/launch_registry.yml --forks 100
+         mkdir -p /tmp/{registry-mirror,registry-private}
+         docker run --rm --volume /tmp/registry-mirror/:/tmp/registry-mirror/ --volume /tmp/registry-private/:/tmp/registry-private/ --volume $KM_HOME:/playbooks/ --volume $KM_HOME/$DEFAULT_INVENTORY_FILE:/etc/ansible/hosts --volume $KM_HOME/../docker-registry:/docker-registry/  --network=host ansible ansible-playbook /playbooks/launch_registry.yml --forks 100
      fi
      
 fi
