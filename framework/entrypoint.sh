906f67fee8132db1024e8de2411852293e35271e
seb <sebastien.cabaniols@hpe.com>
2020-03-24 20:22:16 +0000

    stop writing logs to dev/shm, go to docker logs

--- framework/entrypoint.sh
+++ framework/entrypoint.sh
@@ -5,6 +5,6 @@
     && /tmp/wait-for --timeout=240 broker-2:9093 \
     && /tmp/wait-for --timeout=240 broker-3:9094 \
     && /tmp/wait-for --timeout=240 mosquitto:8883 \
-    && /tmp/wait-for --timeout=240 schemaregistry:8081 -- java -server -Xms4g -Xmx4g -cp target/framework-0.0.1-SNAPSHOT-jar-with-dependencies.jar -XX:+ExitOnOutOfMemoryError -XX:+HeapDumpOnOutOfMemoryError com.hpe.krakenmare.Main &> /dev/shm/logs
+    && /tmp/wait-for --timeout=240 schemaregistry:8081 -- java -server -Xms4g -Xmx4g -cp target/framework-0.0.1-SNAPSHOT-jar-with-dependencies.jar -XX:+ExitOnOutOfMemoryError -XX:+HeapDumpOnOutOfMemoryError com.hpe.krakenmare.Main &
 
 sleep infinity
