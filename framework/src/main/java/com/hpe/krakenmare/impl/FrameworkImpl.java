f7c1221c549b54982f11918c496e5923e7645882
Clément Poulain <clement.poulain@hpe.com>
2020-03-25 14:53:58 +0100

    Back to Redis persistence

--- framework/src/main/java/com/hpe/krakenmare/impl/FrameworkImpl.java
+++ framework/src/main/java/com/hpe/krakenmare/impl/FrameworkImpl.java
@@ -11,15 +11,17 @@ import org.slf4j.LoggerFactory;
 import com.hpe.krakenmare.Main;
 import com.hpe.krakenmare.api.Framework;
 import com.hpe.krakenmare.core.Agent;
-import com.hpe.krakenmare.repositories.AgentMemoryRepository;
+import com.hpe.krakenmare.repositories.AgentRedisRepository;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
 
 public class FrameworkImpl implements Framework {
 
 	public final static Logger LOG = LoggerFactory.getLogger(FrameworkImpl.class);
 
-	// private final Jedis jedis = new Jedis(HostAndPort.parseString(Main.getProperty("redis.server")));
-	// private final AgentRedisRepository agents = new AgentRedisRepository(jedis);
-	private final AgentMemoryRepository agents = new AgentMemoryRepository();
+	private final Jedis jedis = new Jedis(HostAndPort.parseString(Main.getProperty("redis.server")));
+	private final AgentRedisRepository agents = new AgentRedisRepository(jedis);
 	private final FrameworkMqttClient mqttListener = new FrameworkMqttClient();
 
 	private final Producer<String, byte[]> kafkaProducer = KafkaUtils.createByteArrayProducer("framework-manager");
