868860ba917a63320a7699d5b02a6fcff4d61a0c
jeff.hanson <jeff.hanson@hpe.com>
2020-03-30 15:42:55 -0400

    Add check_schemas.sh tool for simulator container

--- simulator/entrypoint.sh
+++ simulator/entrypoint.sh
@@ -4,6 +4,7 @@
 /tmp/wait-for --timeout=240 mosquitto:8883 || exit 1
 /tmp/wait-for --timeout=240 framework:8080 || exit 1
 /tmp/wait-for --timeout=240 schemaregistry:8081 || exit 1
+/simulator/check_schemas.sh || exit 1
 
 cd /simulator
 
