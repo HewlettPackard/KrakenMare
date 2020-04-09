515fe78f8851a7ef8af3f89990a8ea0552ae9b36
jeff.hanson <jeff.hanson@hpe.com>
2020-04-09 11:13:24 -0400

    Add copyright to all files in config-kafka that support comments

--- config-kafka/entrypoint.sh
+++ config-kafka/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# (C) Copyright 2020 Hewlett Packard Enterprise Development LP.
 
 /tmp/wait-for --timeout=240 zookeeper:2181 || exit 1
 /tmp/wait-for --timeout=240 broker-1:29092 || exit 1
