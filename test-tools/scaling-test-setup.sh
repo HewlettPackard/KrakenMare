bdf09aa6f5face5e401bfa97bf885f200e3c201a
jeff.hanson <jeff.hanson@hpe.com>
2020-04-13 10:48:26 -0400

    Change to 10 partitions. Remove max message size adjustment

--- test-tools/scaling-test-setup.sh
+++ test-tools/scaling-test-setup.sh
@@ -10,8 +10,5 @@ export  KAFKA_OPTS="-Djava.security.auth.login.config=/run/secrets/broker_jaas.c
 # To do this we adjust topic to have a size of 9 million, the brokers to support 9 million and producer to support 9 million
 # producer configuration is in /run/secrets/client-sasl_ssl.conf
 kafka-topics --bootstrap-server broker-1:29092 --delete --topic sasl_ssl  --command-config /run/secrets/client-sasl_ssl.conf
-kafka-topics --bootstrap-server broker-1:29092 --create --topic sasl_ssl --partitions 5 --replication-factor 2 --config max.message.bytes=9000000 --command-config /run/secrets/client-sasl_ssl.conf || exit 1
+kafka-topics --bootstrap-server broker-1:29092 --create --topic sasl_ssl --partitions 10 --replication-factor 2 --command-config /run/secrets/client-sasl_ssl.conf || exit 1
 kafka-topics --bootstrap-server broker-1:29092 --topic sasl_ssl --describe --command-config /run/secrets/client-sasl_ssl.conf || exit 1
-
-kafka-configs --bootstrap-server broker-1:29092 --entity-default  --entity-type brokers --alter --add-config message.max.bytes=9000000  --command-config /run/secrets/client-sasl_ssl.conf || exit 1
-kafka-configs --bootstrap-server broker-1:29092 --entity-type brokers --describe --entity-default --command-config /run/secrets/client-sasl_ssl.conf || exit 1
