be61dfbb3d6283bd970e4cd08a00bd93969356f5
jeff.hanson <jeff.hanson@hpe.com>
2020-04-16 17:36:36 -0400

    Upgrade the Apache Druid 0.17.1 tarball

--- druid/entrypoint.sh
+++ druid/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-/tmp/wait-for --timeout=240 broker-1:9092 && \
-/tmp/wait-for --timeout=240 broker-2:9093 && \
-/tmp/wait-for --timeout=240 broker-3:9094 && \
-/tmp/wait-for --timeout=240 zookeeper:2181 -- bin/supervise -c quickstart/tutorial/conf/quickstart-no-zk.conf
+echo "hello from druid... starting waiting for services..."
+
+/tmp/wait-for --timeout=240 broker-1:9092 ||  { echo "timeout on b1" ; exit 1 ;  }
+/tmp/wait-for --timeout=240 broker-2:9093 ||  { echo "timeout on b2" ; exit 1 ;  }
+/tmp/wait-for --timeout=240 broker-3:9094 ||  { echo "timeout on b3" ; exit 1 ;  }
+/tmp/wait-for --timeout=240 schemaregistry:8081 || { echo "timeout on schemaregistry" ; exit 1 ;  }
+
+case $SIZE in
+         nano     ) bin/start-nano-quickstart  ;;
+         micro    ) bin/start-micro-quickstart  ;;
+         small    ) bin/start-single-server-small ;;
+         medium   ) bin/start-single-server-medium ;;
+         large    ) bin/start-single-server-large ;;
+         xlarge   ) bin/start-single-server-xlarge ;;
+         *        ) echo "unrecognized SIZE option" ; exit 1  ;;
+esac
