2bc3b4cad324436085fdd208a46636aef28fa2c4
jeff.hanson <jeff.hanson@hpe.com>
2020-04-16 21:41:52 -0400

    Complete Apache Druid 0.17.1 in KM styled container.  Use environment variable in all-compose.yml to set size

--- config-druid/entrypoint.sh
+++ config-druid/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # (C) Copyright 2020 Hewlett Packard Enterprise Development LP.
 
-/tmp/wait-for --timeout=240 druid:8090 || exit 1
+/tmp/wait-for --timeout=240 druid:8081 || exit 1
 
 # wait for supervisor rest API to be up
-until curl -s druid:8090/druid/indexer/v1/supervisor; do
+until curl -s druid:8081/druid/indexer/v1/supervisor; do
   sleep 1
 done
 
 echo "Load the indexer for simulator data"
-curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/perfquery-index.json http://druid:8090/druid/indexer/v1/supervisor || exit 1
-curl -s druid:8090/druid/indexer/v1/supervisor | jq .
+curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/perfquery-index.json http://druid:8081/druid/indexer/v1/supervisor || exit 1
+curl -s druid:8081/druid/indexer/v1/supervisor | jq .
 echo " Load the indexer for collectd data"
-curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/collectd-index.json http://druid:8090/druid/indexer/v1/supervisor || exit 1
-curl -s druid:8090/druid/indexer/v1/supervisor | jq .
+curl -s -XPOST -H'Content-Type: application/json' -d @/tmp/collectd-index.json http://druid:8081/druid/indexer/v1/supervisor || exit 1
+curl -s druid:8081/druid/indexer/v1/supervisor | jq .
--- druid/entrypoint.sh
+++ druid/entrypoint.sh
@@ -7,6 +7,8 @@ echo "hello from druid... starting waiting for services..."
 /tmp/wait-for --timeout=240 broker-3:9094 ||  { echo "timeout on b3" ; exit 1 ;  }
 /tmp/wait-for --timeout=240 schemaregistry:8081 || { echo "timeout on schemaregistry" ; exit 1 ;  }
 
+export DRUID_SKIP_JAVA_CHECK=1
+
 case $SIZE in
          nano     ) bin/start-nano-quickstart  ;;
          micro    ) bin/start-micro-quickstart  ;;
