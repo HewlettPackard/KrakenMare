bf03f8d90db9ec37025cfbb8fd77ea524953d4fa
Clément Poulain <clement.poulain@hpe.com>
2020-04-20 15:20:01 +0200

    Enable logging for Paho MQTT

--- framework/entrypoint.sh
+++ framework/entrypoint.sh
@@ -7,6 +7,11 @@
     && /tmp/wait-for --timeout=240 broker-2:9093 \
     && /tmp/wait-for --timeout=240 broker-3:9094 \
     && /tmp/wait-for --timeout=240 mosquitto:8883 \
-    && /tmp/wait-for --timeout=240 schemaregistry:8081 -- java -server -Xms${KM_FM_XMS} -Xmx${KM_FM_XMX} -cp target/framework-0.0.1-SNAPSHOT-jar-with-dependencies.jar -XX:+ExitOnOutOfMemoryError -XX:+HeapDumpOnOutOfMemoryError com.hpe.krakenmare.Main &
+    && /tmp/wait-for --timeout=240 schemaregistry:8081 -- \
+        java -server -Xms${KM_FM_XMS} -Xmx${KM_FM_XMX} \
+        -cp target/framework-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+        -XX:+ExitOnOutOfMemoryError -XX:+HeapDumpOnOutOfMemoryError \
+        -Djava.util.logging.config.file=/opt/krakenmare/framework/log.properties \
+        com.hpe.krakenmare.Main &
 
 sleep infinity
