3b774a76596dde6aecb7d594d1482ec72e03c5fd
jeff.hanson <jeff.hanson@hpe.com>
2020-04-21 11:36:42 -0400

    Add feature of KM_DRUID_SIZE_PROFILE to infrastructure and druid. Comment on why we skip java check

--- druid/entrypoint.sh
+++ druid/entrypoint.sh
@@ -7,9 +7,10 @@ echo "hello from druid... starting waiting for services..."
 /tmp/wait-for --timeout=240 broker-3:9094 ||  { echo "timeout on b3" ; exit 1 ;  }
 /tmp/wait-for --timeout=240 schemaregistry:8081 || { echo "timeout on schemaregistry" ; exit 1 ;  }
 
+# Even though Java is in the path and is the right version druid still does not start. Hence we skip the java check.
 export DRUID_SKIP_JAVA_CHECK=1
 
-case $SIZE in
+case $KM_DRUID_SIZE_PROFILE in
          nano     ) bin/start-nano-quickstart  ;;
          micro    ) bin/start-micro-quickstart  ;;
          small    ) bin/start-single-server-small ;;
